8e7cbcfb91cf9671638ace3e4134ee0898b97e5f
Kazi Masum <masum15-12549@diu.edu.bd>
2023-07-14 23:09:50 +0600

    version 0.2

--- lib/screens/general/checkout_screen.dart
+++ lib/screens/general/checkout_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CheckOutScreen extends StatefulWidget {
-  const CheckOutScreen({super.key});
+  const CheckOutScreen({Key? key}) : super(key: key);
 
   @override
   _CheckOutScreenState createState() => _CheckOutScreenState();
@@ -13,7 +13,15 @@ class _CheckOutScreenState extends State<CheckOutScreen> {
   String accountNumber = '';
   String accountName = '';
   double amount = 0.0;
+  String? selectedCard;
   bool isPaymentSuccessful = false;
+
+  List<String> paymentCards = [
+    'https://cdn.icon-icons.com/icons2/1259/PNG/512/1495815261-jd08_84586.png',
+    'https://cdn.icon-icons.com/icons2/1259/PNG/512/1495815236-jd07_84584.png',
+    'https://cdn.icon-icons.com/icons2/1259/PNG/512/1495815276-jd05_84594.png',
+    'https://cdn.icon-icons.com/icons2/1259/PNG/512/1495815234-jd03_84587.png',
+  ];
 
   void makePayment() {
     if (_formKey.currentState!.validate()) {
@@ -34,90 +42,165 @@ class _CheckOutScreenState extends State<CheckOutScreen> {
         title: Text('Payment Gateway'),
         backgroundColor: Colors.blue,
       ),
-      body: isPaymentSuccessful
-          ? Text(
-        'Payment Successful',
-        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-      )
-          : Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Container(
+          child: isPaymentSuccessful
+              ? Align(
+                child: Center(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 180,
+                      ),
+                      CircleAvatar(
+                        radius: 50,
+                          backgroundColor: Colors.blue,
+                          child: CircleAvatar(
+                              radius: 48,
+                              backgroundColor: Colors.white,
+                              child: Icon(Icons.done,color: Colors.green,size: 60,))),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Text(
+            'Payment Successful',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+                    ],
+                  ),
+                ),
+              )
+              : Column(
             children: [
-              TextFormField(
-                validator: (value) {
-                  if (value == null || value.isEmpty ) {
-                    return 'Please enter the bank account number';
-                  }
-                  return null;
-                },
-                onChanged: (value) {
-                  setState(() {
-                    accountNumber = value;
-                  });
-                },
-                decoration: InputDecoration(
-                  labelText: 'Bank Account Number',
-                  border: OutlineInputBorder(),
+              Container(
+                width: 700,
+                height: 60,
+
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: paymentCards.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (BuildContext context, int index) {
+                    final cardImageUrl = paymentCards[index];
+                    final isSelected = selectedCard == cardImageUrl;
+
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedCard = cardImageUrl;
+                        });
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          width: 80,
+                          height: 50,
+
+                          decoration: BoxDecoration(
+                            color: isSelected ? Colors.blue.withOpacity(0.8) : Colors.transparent,
+
+                                borderRadius: BorderRadius.circular(16),
+                                border: Border.all(color: Colors.blue)
+
+                            
+                          ),
+                          child: Image.network(cardImageUrl),
+                        ),
+                      ),
+                    );
+                  },
                 ),
               ),
-              SizedBox(height: 16.0),
-              TextFormField(
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter the account name';
-                  }
-                  return null;
-                },
-                onChanged: (value) {
-                  setState(() {
-                    accountName = value;
-                  });
-                },
-                decoration: InputDecoration(
-                  labelText: 'Account Name',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 16.0),
-              TextFormField(
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter the amount';
-                  }
-                  if (double.tryParse(value) == null) {
-                    return 'Please enter a valid amount';
-                  }
-                  return null;
-                },
-                onChanged: (value) {
-                  setState(() {
-                    amount = double.tryParse(value) ?? 0.0;
-                  });
-                },
-                decoration: InputDecoration(
-                  labelText: 'Amount',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: makePayment,
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.blue,
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 32,
-                    vertical: 16,
+              Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+
+                        TextFormField(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter the  account number';
+                            }
+                            return null;
+                          },
+                          onChanged: (value) {
+                            setState(() {
+                              accountNumber = value;
+                            });
+                          },
+                          decoration: InputDecoration(
+                            labelText: 'Account Number',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        SizedBox(height: 16.0),
+                        TextFormField(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter the account name';
+                            }
+                            return null;
+                          },
+                          onChanged: (value) {
+                            setState(() {
+                              accountName = value;
+                            });
+                          },
+                          decoration: InputDecoration(
+                            labelText: 'Account Name',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        SizedBox(height: 16.0),
+                        TextFormField(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter the amount';
+                            }
+                            if (double.tryParse(value) == null) {
+                              return 'Please enter a valid amount';
+                            }
+                            return null;
+                          },
+                          onChanged: (value) {
+                            setState(() {
+                              amount = double.tryParse(value) ?? 0.0;
+                            });
+                          },
+                          decoration: InputDecoration(
+                            labelText: 'Amount',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        SizedBox(height: 16.0),
+
+
+                        SizedBox(height: 16.0),
+                        ElevatedButton(
+                          onPressed: makePayment,
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.blue,
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 32,
+                              vertical: 16,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                          child: Text(
+                            'Make Payment',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                ),
-                child: Text(
-                  'Make Payment',
-                  style: TextStyle(fontSize: 18),
                 ),
               ),
             ],
